6f69d19e826eeb2170a716e949e0ddcd2f195d14
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-27 19:09:20 +0530

    api keys added

---
+++ contracts/Market.sol
@@ -0,0 +1,9 @@
+
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,37 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+// const fs = require('fs');
+// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+const infuraId = process.env.STAGING_ALCHEMY_KEY
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    
+    mumbai: {
+      // Infura
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${infuraId}`,
+      // url: "https://rpc-mumbai.matic.today",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    // matic: {
+    //   // Infura
+    //   url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+    //   // url: "https://rpc-mainnet.maticvigil.com",
+    //   accounts: [process.env.PRIVATE_KEY]
+    // }
+  
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
